c74f555fbfb96077b51186076e3d899bd2d45894
= <greggjs@miamioh.edu>
2014-01-15 23:52:12 +0000

    Client now works

--- manage.py
+++ manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     from django.core.management import execute_from_command_line
-
+    sys.path.append('/env/sessions/sessions-model')
+    sys.path.append('/env/sessions/users-model')
     execute_from_command_line(sys.argv)
