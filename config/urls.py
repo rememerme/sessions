d84da9650b6a133ec66ae4c8b222052520ec551c
= <greggjs@miamioh.edu>
2014-01-18 09:36:20 +0000

    Urls yayayayayay

--- config/urls.py
+++ config/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^rest/v1/sessions/docs/', include('rest_framework_swagger.urls')),
-    url(r'^rest/v1/sessions/', include('rememerme.sessions.rest.urls'))
+    url(r'^rest/v1/sessions', include('rememerme.sessions.rest.urls'))
 )
