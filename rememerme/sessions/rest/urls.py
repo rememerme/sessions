7115a23c05a81ee3e9c3dd025d87c2604d734cab
= <greggjs@miamioh.edu>
2014-01-18 09:33:19 +0000

    Added rest framework static and fixed trailing slash in urls

--- rememerme/sessions/rest/urls.py
+++ rememerme/sessions/rest/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 from rememerme.sessions.rest import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.SessionsListView.as_view()),
+    url(r'^/?$', views.SessionsListView.as_view()),
     url(r'^/(?P<session_id>[-\w]+)/?$', views.SessionsSingleView.as_view()),
 )
